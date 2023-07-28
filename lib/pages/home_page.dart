c2c8f4868c951fd4309eee8d985fe4354b90cf87
omkar <pendkalkar.omkar786@gmail.com>
2023-07-28 20:48:10 +0530

    Commited to just save progress

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -6,7 +7,39 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Center(child:Text("Lohbdhc n kjsnajskx n cjksn", style: TextStyle(color: Theme.of(context).colorScheme.secondary),)),
+      appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          elevation: 1,
+          title: RichText(
+            maxLines: 1,
+            text: TextSpan(
+              text:'PRIME',
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.tertiary,
+                  fontFamily: 'Aquire-Regular',
+                  fontSize: 18),
+              children:<TextSpan>[
+                TextSpan(
+                  text:'VPN',
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.secondary,
+                      fontFamily: 'Montserrat-Regular',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 18),
+                ),
+              ],
+            ),
+          )),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+
+          ],
+        ),
+        ),
+      ),
     );
   }
 }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
-    fontFamily: 'Monteserrat-Regular',
+    fontFamily: 'Cerco',
     colorScheme:const  ColorScheme.dark(
       brightness: Brightness.dark,
       primary:Color(0xFF6B7DEB),
       secondary:  Color(0xFFFFFFFF),
+      tertiary: Color(0xFFECCA2E),
       error: Color(0xFFD81003),
       background: Color(0xFF12122E),
       onBackground: Color(0xFF1F1F47),
@@ -17,11 +18,12 @@ ThemeData darkTheme = ThemeData(
 
 ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
-    fontFamily: 'Monteserrat-Regular',
+    fontFamily: 'Cerco',
     colorScheme:const  ColorScheme.light(
       brightness: Brightness.light,
       primary:Color(0xFF6B7DEB),
       secondary:  Color(0xFF000000),
+      tertiary: Color(0xFFEEBD01),
       error: Color(0xFFD81003),
       background: Color(0xFFFFFFFF),
       onBackground: Color(0x801F1F47),
